ebcd86218a846e53a5fd64a90bbccf25b1914e08
ThibeauWouters <thibeauwouters@gmail.com>
2024-08-04 09:49:56 +0200

    Uploading the failed runs

---
+++ batch_4d_runs/failed_outdir/out_bbh_3/submit/analysis_bbh_3_0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=0_bbh_3
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=24:00:00
+#SBATCH --output=outdir/out_bbh_3/log_data_analysis/0_bbh_3_%j.log
+
+#SBATCH --mem-per-cpu=2G
+
+
+
+mpirun parallel_bilby_analysis outdir/out_bbh_3/data/bbh_3_data_dump.pickle --nact 20 --label bbh_3_0 --sampling-seed 554290 --outdir /home/urash/twouters/projects/jim_meets_harmonic/batch_4d_runs/outdir/out_bbh_3/result
