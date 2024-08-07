257f78c7c6d423e393536f5096a1b116442a9cbc
ThibeauWouters <thibeauwouters@gmail.com>
2024-08-07 14:38:19 +0200

    Test run 11d; running
    Test run is running, but still need to check the results once they are in. Note that we use the default aligned spins BBH prior from bilby

---
+++ 11d_runs/my_batch_generation.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Call parallel_bilby_generation on all the injections without SLURM hassle
+
+N=1
+for i in $(seq 0 $((N-1)) )
+do
+    echo "========== parallel_bilby_generation injection " $i
+    parallel_bilby_generation bbh_${i}.ini &> generation_log/generation_injection_${i}.err
+done
