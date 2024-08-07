84aff0cfbfaf46a010d7e497b4c6bf95d7bc3e04
ThibeauWouters <thibeauwouters@gmail.com>
2024-08-07 09:22:59 +0200

    Moving around files

--- batch_4d_runs/failed_outdir_RB/out_bbh_3/submit/bash_bbh_3.sh
+++
@@ -1,2 +0,0 @@
-jid0=$(sbatch outdir/out_bbh_3/submit/analysis_bbh_3_0.sh)
-squeue -u $USER -o "%u %.10j %.8A %.4C %.40E %R"
--- batch_4d_runs/failed_outdir_no_RB/out_bbh_1/submit/bash_bbh_1.sh
+++
@@ -1,2 +0,0 @@
-jid0=$(sbatch outdir/out_bbh_1/submit/analysis_bbh_1_0.sh)
-squeue -u $USER -o "%u %.10j %.8A %.4C %.40E %R"
